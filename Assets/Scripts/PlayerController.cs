f8b9e961ec028add50ca0573f762c617100d6f4a
Klarrc <kevindanaclark@gmail.com>
2023-01-26 19:54:50 -0500

    text
    added all the text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,44 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+// Include the namespace required to use Unity UI and Input System
 using UnityEngine.InputSystem;
+using TMPro;
 
-public class PlayerController : MonoBehaviour
-{
-    public float speed = 0;
+public class PlayerController : MonoBehaviour {
+	
 
-    private Rigidbody rb;
+	public float speed;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
 
-    private float movementX;
-    private float movementY;
+        private float movementX;
+        private float movementY;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private Rigidbody rb;
+	private int count;
 
-    private void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
 
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+		// Set the count to zero 
+		count = 0;
 
-        rb.AddForce(movement * speed);
-    }
+		SetCountText ();
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.CompareTag("Pickup"))
+                
+                winTextObject.SetActive(false);
+	}
+
+	void FixedUpdate ()
+	{
+		// Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
+		Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+
+		if (other.gameObject.CompareTag ("Pickup"))
+		{
+			other.gameObject.SetActive (false);
+
+			// Add one to the score variable 'count'
+			count = count + 1;
+
+
+			SetCountText ();
+		}
+	}
+
+        void OnMove(InputValue value)
         {
-            other.gameObject.SetActive(false);
-        }
-    }
+        	Vector2 v = value.Get<Vector2>();
 
+        	movementX = v.x;
+        	movementY = v.y;
+        }
+
+        void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 12) 
+		{
+                    // Set the text value of your 'winText'
+                    winTextObject.SetActive(true);
+		}
+	}
 }
